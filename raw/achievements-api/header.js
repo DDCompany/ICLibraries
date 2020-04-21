86e407c4b12141156e42b2528533832d53a9addf
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-04-21 20:57:25 +0300

    Added LIBRARY macros

--- raw/achievements-api/header.js
+++ raw/achievements-api/header.js
@@ -1,3 +1,10 @@
+LIBRARY({
+    name: "AchievementsAPI",
+    version: 0,
+    shared: true,
+    api: "CoreEngine"
+});
+
 let Color = android.graphics.Color;
 let Bitmap = android.graphics.Bitmap;
 let Canvas = android.graphics.Canvas;
