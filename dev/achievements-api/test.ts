63230e1a51e03ab5ff201d117ba8b807d0a9fc46
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-10-01 23:01:40 +0300

    Changed tests

--- dev/achievements-api/test.ts
+++ dev/achievements-api/test.ts
@@ -1,60 +1,80 @@
+import ItemUseCoordinates = Callback.ItemUseCoordinates;
+
 IMPORT("AchievementsAPI");
 
 AchievementAPI.registerGroup({
-    unique: "test_group",
-    name: "achievements.test",
-    background: "groups_bg.end",
-    icon: VanillaBlockID.bedrock
+    unique: "world_of_colors",
+    name: "achievements.world_of_colors",
+    background: "groups_bg.husbandry",
+    icon: {
+        id: VanillaBlockID.wool,
+        data: 3
+    }
 });
 
-AchievementAPI.registerAll("test_group", [
+AchievementAPI.registerAll("world_of_colors", [
     {
-        unique: "second",
-        column: 3,
-        row: 1,
-        name: "achievements.test.second",
-        item: VanillaItemID.diamond_pickaxe
-    },
-    {
-        unique: "first",
-        parent: "test_group:second",
-        column: 2,
+        unique: "welcome",
+        column: 0,
         row: 0,
-        connection: Connection.VERTICAL,
-        name: "achievements.test.first",
-        item: VanillaItemID.apple
+        name: "achievements.world_of_colors.welcome",
+        item: VanillaItemID.ender_pearl
     },
     {
-        unique: "third",
-        parent: "second",
-        strongDependence: true,
-        column: 4,
-        row: 2,
+        unique: `glass`,
+        type: AchievementType.goal,
+        column: 1,
+        row: 3,
+        parent: "welcome",
+        name: `achievements.world_of_colors.glass`,
+        description: "achievements.world_of_colors.glass_desc",
+        progressMax: 16,
         connection: Connection.VERTICAL,
-        name: "achievements.test.third",
-        description: "achievements.test.third_desc",
-        item: VanillaItemID.diamond
+        item: VanillaBlockID.stained_glass
     }
 ]);
-Translation.addTranslation("achievements.test", {en: "Test"});
-Translation.addTranslation("achievements.test.first", {en: "First Achievement"});
-Translation.addTranslation("achievements.test.second", {en: "Second Achievement"});
-Translation.addTranslation("achievements.test.third", {en: "Third Achievement"});
-Translation.addTranslation("achievements.test.third_desc", {en: "Sample of description"});
 
-Callback.addCallback("ItemUse", function () {
-    if (!AchievementAPI.isCompleted("test_group", "first")) {
-        AchievementAPI.give("test_group", "first");
-        return;
-    }
+for (let i = 0; i < 16; i++) {
+    AchievementAPI.register("world_of_colors", {
+        unique: `wool${i}`,
+        column: 1 + i % 6,
+        row: Math.floor(i / 6),
+        parent: "welcome",
+        name: `achievements.world_of_colors.wool`,
+        description: "achievements.world_of_colors.wool_desc",
+        connection: Connection.VERTICAL,
+        item: {
+            id: VanillaBlockID.wool,
+            data: i
+        }
+    });
+}
 
-    if (!AchievementAPI.isCompleted("test_group", "second")) {
-        AchievementAPI.give("test_group", "second");
-        return;
-    }
+Translation.addTranslation("achievements.world_of_colors", {en: "World of Colors"});
+Translation.addTranslation("achievements.world_of_colors.welcome", {en: "Welcome to the Club"});
+Translation.addTranslation("achievements.world_of_colors.wool", {en: "Wool"});
+Translation.addTranslation("achievements.world_of_colors.wool_desc", {en: "Place the block of Wool"});
+Translation.addTranslation("achievements.world_of_colors.glass", {en: "Glass"});
+Translation.addTranslation("achievements.world_of_colors.glass_desc", {en: "Place all colors of Glass"});
 
-    if (!AchievementAPI.isCompleted("test_group", "third")) {
-        AchievementAPI.give("test_group", "third");
-        return;
+Callback.addCallback("LevelLoaded", () =>
+    AchievementAPI.give("world_of_colors", "welcome"));
+
+Callback.addCallback("ItemUse", (coords: ItemUseCoordinates, item: ItemInstance) => {
+    switch (item.id) {
+        case VanillaBlockID.wool:
+            AchievementAPI.give("world_of_colors", `wool${item.data}`);
+            break;
+        case VanillaBlockID.stained_glass:
+            const achievement =
+                AchievementAPI
+                    .getGroup("world_of_colors")
+                    .getChild("glass");
+            const data = achievement.getData();
+            if (!data[item.data]) {
+                data[item.data] = true;
+                achievement.give();
+            }
+            break;
     }
 });
